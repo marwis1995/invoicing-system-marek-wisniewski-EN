7a352f6c3eb7280ec6ca5b2c184a4955afa1201f
marwis1995 <119961820+marwis1995@users.noreply.github.com>
2023-09-19 10:14:10 +0200

    Lesson2 (#3)
    * lesson2
    
    * lesson2 - gradle-wrapper
    
    * lesson2
    
    * lesson2
    
    * lesson2
    
    * lesson2
    
    * lesson2
    
    * lesson2
    
    * lesson2
    
    * lesson2
    
    ---------
    
    Co-authored-by: Marek Wisniewski <a@a.pl>

---
+++ app/src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -0,0 +1,17 @@
+package pl.futurecollars.invoicing.model;
+
+import lombok.Data;
+
+@Data
+public class Company {
+
+  private String taxIdentification;
+  private String address;
+  private String name;
+
+  public Company(String taxIdentification, String address, String name) {
+    this.taxIdentification = taxIdentification;
+    this.address = address;
+    this.name = name;
+  }
+}
