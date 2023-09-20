565740489163da4ad4c0991a7bd63ab9583916b0
Marek Wisniewski <a@a.pl>
2023-09-20 21:33:53 +0200

    lesson3

--- app/src/main/java/pl/futurecollars/invoicing/model/Invoice.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Invoice.java
@@ -2,7 +2,6 @@ package pl.futurecollars.invoicing.model;
 
 import java.time.LocalDate;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
