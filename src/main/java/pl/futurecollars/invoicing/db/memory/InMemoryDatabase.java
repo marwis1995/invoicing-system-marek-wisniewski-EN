db04aff1d16ff8957a11268efea5f36958216ab2
Marek Wisniewski <a@a.pl>
2023-10-11 22:05:40 +0200

    lesson7

--- src/main/java/pl/futurecollars/invoicing/db/memory/InMemoryDatabase.java
+++ src/main/java/pl/futurecollars/invoicing/db/memory/InMemoryDatabase.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import org.springframework.stereotype.Repository;
 import pl.futurecollars.invoicing.db.Database;
 import pl.futurecollars.invoicing.model.Invoice;
 
