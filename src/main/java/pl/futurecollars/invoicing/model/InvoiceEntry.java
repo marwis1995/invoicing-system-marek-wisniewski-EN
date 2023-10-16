0bbdbfa5736130b9eebf33215d04bb053f3a2451
Marek Wisniewski <a@a.pl>
2023-10-16 21:21:07 +0200

    lesson8

--- src/main/java/pl/futurecollars/invoicing/model/InvoiceEntry.java
+++ src/main/java/pl/futurecollars/invoicing/model/InvoiceEntry.java
@@ -1,5 +1,6 @@
 package pl.futurecollars.invoicing.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,10 +11,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class InvoiceEntry {
 
+  @ApiModelProperty(value = "Product/service description", required = true, example = "Dell laptop")
   private String description;
+  @ApiModelProperty(value = "Number of items", required = true, example = "85")
   private int quantity;
+  @ApiModelProperty(value = "Product/service price", required = true, example = "1234.12")
   private BigDecimal price;
+  @ApiModelProperty(value = "Product/service tax value", required = true, example = "123.45")
   private BigDecimal vatValue;
+  @ApiModelProperty(value = "Tax rate", required = true)
   private Vat vatRate;
 
 }
