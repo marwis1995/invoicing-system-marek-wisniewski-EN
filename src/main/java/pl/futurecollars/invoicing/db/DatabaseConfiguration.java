bac52ec490bcdbe597cf38bfb208c84d49fa2a2e
marwis1995 <119961820+marwis1995@users.noreply.github.com>
2023-11-06 18:28:58 +0100

    Lesson12 (#14)
    * lesson12
    
    * lesson12
    
    * lesson12
    
    * lesson12
    
    ---------
    
    Co-authored-by: Marek Wisniewski <a@a.pl>

--- src/main/java/pl/futurecollars/invoicing/db/DatabaseConfiguration.java
+++ src/main/java/pl/futurecollars/invoicing/db/DatabaseConfiguration.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import pl.futurecollars.invoicing.db.file.FileBasedDatabase;
 import pl.futurecollars.invoicing.db.file.IdProvider;
 import pl.futurecollars.invoicing.db.memory.InMemoryDatabase;
+import pl.futurecollars.invoicing.db.sql.SqlDatabase;
 import pl.futurecollars.invoicing.utils.FilesService;
 import pl.futurecollars.invoicing.utils.JsonService;
 
@@ -48,4 +50,9 @@ public class DatabaseConfiguration {
     return new InMemoryDatabase();
   }
 
+  @Bean
+  @ConditionalOnProperty(name = "invoicing-system.database", havingValue = "sql")
+  public Database sqlDatabase(JdbcTemplate jdbcTemplate) {
+    return new SqlDatabase(jdbcTemplate);
+  }
 }
