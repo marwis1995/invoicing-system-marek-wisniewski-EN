942f12559226bfb6d0ce9e75b740d57847ae77b9
Marek Wisniewski <a@a.pl>
2023-11-15 20:41:59 +0100

    lesson15

--- src/main/java/pl/futurecollars/invoicing/db/jpa/CompanyRepository.java
+++ src/main/java/pl/futurecollars/invoicing/db/jpa/CompanyRepository.java
@@ -2,9 +2,9 @@ package pl.futurecollars.invoicing.db.jpa;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import pl.futurecollars.invoicing.model.Invoice;
+import pl.futurecollars.invoicing.model.Company;
 
 @Repository
-public interface InvoiceRepository extends CrudRepository<Invoice, Long> {
+public interface CompanyRepository extends CrudRepository<Company, Long> {
 
 }
---
+++ src/main/java/pl/futurecollars/invoicing/model/WithId.java
@@ -0,0 +1,8 @@
+package pl.futurecollars.invoicing.model;
+
+public interface WithId {
+
+  Long getId();
+  void setId(Long id);
+
+}
